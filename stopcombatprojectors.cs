cadab92c20a0c258188e8eb94705d04e9e53e981
cybercheese1 <62494391+cybercheese1@users.noreply.github.com>
2020-03-22 00:23:38 -0500

    Add files via upload

--- stopcombatprojectors.cs
+++ stopcombatprojectors.cs
@@ -17,95 +17,73 @@ using VRageMath;
 using VRage.Game.Components;
 //using Ingame = Sandbox.ModAPI.Ingame;
 
-namespace StopCombatProjector
-{
-    [VRage.Game.Components.MyEntityComponentDescriptor(typeof(Sandbox.Common.ObjectBuilders.MyObjectBuilder_Projector), true)]
+namespace StopCombatProjector {
+  [VRage.Game.Components.MyEntityComponentDescriptor(typeof (Sandbox.Common.ObjectBuilders.MyObjectBuilder_Projector), true)]
 
-    public class StopCombatProjector : MyGameLogicComponent
-    {
+  public class StopCombatProjector: MyGameLogicComponent {
 
-        private VRage.ObjectBuilders.MyObjectBuilder_EntityBase _objectBuilder;
-        private DateTime lastUpdate = DateTime.MinValue;
-        private IMyProjector projector;
+    private VRage.ObjectBuilders.MyObjectBuilder_EntityBase _objectBuilder;
+    private DateTime lastUpdate = DateTime.MinValue;
+    private IMyProjector projector;
 
-        public override void Close()
-        {
+    public override void Close() {
 
-        }
-
-        public override void Init(VRage.ObjectBuilders.MyObjectBuilder_EntityBase objectBuilder)
-        {
-            _objectBuilder = objectBuilder;
-            projector = (Entity as IMyProjector);
-
-             if (projector != null && projector.BlockDefinition.ToString().Contains("projector"))
-               Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
-        }
-
-        public override void MarkForClose()
-        {
-        }
-
-        public override void UpdateAfterSimulation()
-        {
-        }
-
-        public override void UpdateBeforeSimulation()
-        {
-        }
-
-        public override void UpdateAfterSimulation100()
-        {
-        }
-
-        public override void UpdateAfterSimulation10()
-        {
-            try
-            {
-
-                if (projector == null || !projector.Enabled || !projector.IsWorking || !projector.IsFunctional) return;
-                var projectorgrid = projector.GetTopMostParent();
-                VRage.Game.ModAPI.IMyCubeGrid grid = (projectorgrid as VRage.Game.ModAPI.IMyCubeGrid);
-                if (projectorgrid.Physics.LinearVelocity.Length() > 1)
-                {
-                    
-                    List<VRage.Game.ModAPI.IMySlimBlock> blocks = new List<VRage.Game.ModAPI.IMySlimBlock>();
-                    grid.GetBlocks(blocks, b => b != null && b.FatBlock != null && (b.FatBlock as IMyProjector) != null && b.FatBlock.IsWorking && b.FatBlock.IsFunctional &&
-                            b.FatBlock.BlockDefinition.ToString().Contains("MyObjectBuilder_Projector"));
-
-                    foreach (VRage.Game.ModAPI.IMySlimBlock b in blocks)
-                    {
-                        if ((b.FatBlock as IMyProjector).Enabled)
-                            (b.FatBlock as IMyProjector).RequestEnable(false);
-							 var damage = grid.GridSizeEnum.Equals(MyCubeSize.Large) ? 15.5f : 0.05f;
-							 b.DecreaseMountLevel(damage, null, true);
-							 b.ApplyAccumulatedDamage();
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                
-            }
- 
-        }
-
-        public override void UpdateBeforeSimulation10()
-        {
-        }
-
-        public override void UpdateBeforeSimulation100()
-        {
-
-        }
-
-        public override void UpdateOnceBeforeFrame()
-        {
-        }
-
-        public override VRage.ObjectBuilders.MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
-        {
-            return _objectBuilder;
-        }
     }
+
+    public override void Init(VRage.ObjectBuilders.MyObjectBuilder_EntityBase objectBuilder) {
+      _objectBuilder = objectBuilder;
+      projector = (Entity as IMyProjector);
+
+      if (projector != null && projector.BlockDefinition.ToString().Contains("projector"))
+        Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
+
+    }
+
+    public override void MarkForClose() {}
+
+    public override void UpdateAfterSimulation() {}
+
+    public override void UpdateBeforeSimulation() {}
+
+    public override void UpdateAfterSimulation100() {}
+
+    public override void UpdateAfterSimulation10() {
+      try {
+
+        if (projector == null || !projector.Enabled || !projector.IsWorking || !projector.IsFunctional) return;
+        var projectorgrid = projector.GetTopMostParent();
+        VRage.Game.ModAPI.IMyCubeGrid grid = (projectorgrid as VRage.Game.ModAPI.IMyCubeGrid);
+        if (projectorgrid.Physics.LinearVelocity.Length() > 1) {
+
+          List < VRage.Game.ModAPI.IMySlimBlock > blocks = new List < VRage.Game.ModAPI.IMySlimBlock > ();
+          grid.GetBlocks(blocks, b => b != null && b.FatBlock != null && (b.FatBlock as IMyProjector) != null && b.FatBlock.IsWorking && b.FatBlock.IsFunctional &&
+            b.FatBlock.BlockDefinition.ToString().Contains("MyObjectBuilder_Projector"));
+
+          foreach(VRage.Game.ModAPI.IMySlimBlock b in blocks) {
+            if ((b.FatBlock as IMyProjector).Enabled)
+              (b.FatBlock as IMyProjector).RequestEnable(false);
+            var damage = grid.GridSizeEnum.Equals(MyCubeSize.Large) ? 15.5 f : 0.05 f;
+            b.DecreaseMountLevel(damage, null, true);
+            b.ApplyAccumulatedDamage();
+            MyAPIGateway.Utilities.ShowMessage(grid.CustomName, "projector failed because of high velocity!");
+          }
+        }
+      } catch (Exception e) {
+
+      }
+
+    }
+
+    public override void UpdateBeforeSimulation10() {}
+
+    public override void UpdateBeforeSimulation100() {
+
+    }
+
+    public override void UpdateOnceBeforeFrame() {}
+
+    public override VRage.ObjectBuilders.MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false) {
+      return _objectBuilder;
+    }
+  }
 }
